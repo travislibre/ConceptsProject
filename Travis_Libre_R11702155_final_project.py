64a8be43be5e7355b6ed2db3ac14734503d7ab95
Travis Libre <travisjlibre@gmail.com>
2022-12-17 21:52:29 -0600

    Test

--- Travis_Libre_R11702155_final_project.py
+++ Travis_Libre_R11702155_final_project.py
@@ -5,96 +5,105 @@
 # Outputs a .dat file consisting of the input matrix after 100 iterations of the rowCheck function
 import argparse
 from multiprocessing import Pool
-import time
 
 def main():
-    # Parse arguments
-    argumentParse = argparse.ArgumentParser(prog='Concepts Project', description =
-    'Fulfills final project requirements via multithreading')
-    argumentParse.add_argument('-i', "--inputFile", required=True)
-    argumentParse.add_argument('-o', "--outputFile", required=True)
-    argumentParse.add_argument('-t', "--threadCount", type=int, default=1, required=False)
-    commandLineArgs = argumentParse.parse_args()
-    array = []
+    # Process Args
+    commandLineArgs = takeArgs()
+    dataArray = []
     input_file = open(commandLineArgs.inputFile, 'r')
+    # Read & Convert file
+    readFile(input_file, dataArray)
+    height = len(dataArray)
+    width = len(dataArray[0])
+    poolData = list()
+    package(poolData, dataArray, height, width)
     threads = commandLineArgs.threadCount
     # Catches thread<=0 error
     if threads < 1:
         raise Exception("Threads cannot be zero or negative")
     processPool = Pool(processes=threads)
-    poolData = list()
-    # Removes the \n in every line and appends to array
-    for (line) in input_file:
-        line = line[:-1]
-        array.append(list(line))
-    # Accounts for empty line in test files
-    height = len(array) - 1
-    width = len(array[0])
-    print(height, width)
-    count = 100
-    # Package the data into 3 lines
-    # The function defined later in the program only calculates the output for the middle line!
-    for rowIndex in range(height):
-        subArray = list()
-        subArray.append(array[(rowIndex-1)%height])
-        subArray.append(array[(rowIndex)])
-        subArray.append(array[(rowIndex + 1) % height])
-        # Append package to poolData
-        poolData.append(subArray)
-    while count != 0:
+    iterations = 100
+    # Repeat 100 times
+    for count in range(iterations):
         # Store rowCheck result to nextArray
         nextArray = processPool.map(rowCheck , poolData)
-        for i in range(height):
-            # Update poolData
-            poolData[i][0] = nextArray[(i - 1)%height]
-            poolData[i][1] = nextArray[i]
-            poolData[i][2] = nextArray[(i + 1) % height]
-        count = count-1
+        del poolData
+        poolData = list()
+        # Restart poolData
+        package(poolData, nextArray, height, width)
     # Write to output
     output_file = open(commandLineArgs.outputFile, 'w')
+    writeToOutput(output_file, height, width, nextArray)
+
+# Takes file dimensions, output_file, and the array to be printed, prints array to output file
+def writeToOutput(output_file, height, width, array):
     for i in range(height):
         for j in range(width):
-            output_file.write(nextArray[i][j])
+            num = array[i][j]
+            if(num==1):
+                output_file.write('+')
+            else:
+                output_file.write('-')
         output_file.write('\n')
+
+# Reads file, converts to digits
+# Takes input_file and output matrix
+def readFile(input_file, output):
+    # Removes the \n in every line and appends to dataArray
+    for line in input_file:
+        line = list(line.strip('\n'))
+        # Converts to ints for faster computation
+        for x in range(len(line)):
+            if(line[x]=='-'):
+                line[x] = 0
+            elif(line[x]=='+'):
+                line[x] = 1
+            else:
+                print("Invalid Character Detected")
+        # Accounts for empty line in test files
+        if(len(line)!=0):
+            output.append(line)
+# Parse arguments
+# Takes no arguments, returns argument parse object
+def takeArgs():
+    argumentParse = argparse.ArgumentParser(prog='Concepts Project', description=
+    'Fulfills final project requirements via multithreading')
+    argumentParse.add_argument('-i', "--inputFile", required=True)
+    argumentParse.add_argument('-o', "--outputFile", required=True)
+    argumentParse.add_argument('-t', "--threadCount", type=int, default=1, required=False)
+    commandLineArgs = argumentParse.parse_args()
+    return commandLineArgs
+
+# Package the middle row, it's immediate wrapping neighbors, and width
+# Function requires height in order to wrap data, but does not package height itself
+def package(poolData, dataArray, h, w):
+    for (rowIndex) in range(h):
+        poolData.append([dataArray[(rowIndex - 1) % h], dataArray[(rowIndex)], dataArray[(rowIndex + 1) % h], w])
 
 # rowCheck: takes input, a 2D array of three lines, and returns the middle line with corrected values.
 # rowCheck assumes vertical wraparound has already been accounted for
 def rowCheck(array):
     i=1
-    width = len(array[0])
     # Array to return
-    nextArray=[None]*width
-    for j in range(width):
-        neighbor_sum = 0
+    nextArray=[None]*array[3]
+    for j in range(array[3]):
         up = (i - 1)
         down = (i + 1)
-        left = (j - 1) % width
-        right = (j + 1) % width
-        if array[up][left] == '+':
-            neighbor_sum += 1
-        if array[up][j] == '+':
-            neighbor_sum += 1
-        if array[up][right] == '+':
-            neighbor_sum += 1
-        if array[i][left] == '+':
-            neighbor_sum += 1
-        if array[i][right] == '+':
-            neighbor_sum += 1
-        if array[down][left] == '+':
-            neighbor_sum += 1
-        if array[down][j] == '+':
-            neighbor_sum += 1
-        if array[down][right] == '+':
-            neighbor_sum += 1
-        if array[i][j] == '+':
-            if neighbor_sum == 0 or (neighbor_sum % 2) == 1 or neighbor_sum == 8:
-                nextArray[j] = '-'
+        left = (j - 1) % array[3]
+        right = ((j + 1) % array[3])
+        sum1=(array[up][left]+array[up][j]+array[up][right])
+        sum2=array[i][left]+array[i][right]
+        sum3=array[down][left]+array[down][j]+array[down][right]
+        sums = sum1+sum2+sum3
+        if array[i][j] == 1:
+            if sums == 0 or (sums % 2) == 1 or sums == 8:
+                nextArray[j] = 0
             else:
-                nextArray[j] = '+'
-        elif array[i][j] == '-' and (neighbor_sum == 2 or neighbor_sum == 3 or neighbor_sum == 5 or neighbor_sum == 7):
-            nextArray[j] = '+'
+                nextArray[j] = 1
+        elif sums == 2 or sums == 3 or sums == 5 or sums == 7:
+            nextArray[j] = 1
         else:
-            nextArray[j] = '-'
+            nextArray[j] = 0
     return nextArray
 
 if __name__ == "__main__":
